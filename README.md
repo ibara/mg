97df2273b20c4e638f36e9d6f396d1ef1a435e1c
Brian Callahan <dodonpachi-github@mailinator.com>
2016-11-05 00:01:30 -0400

    Small update to latest mg code.

--- README.md
+++ README.md
@@ -29,4 +29,4 @@ All licenses are ISC or BSD.
 
 Get a tarball
 -------------
-http://devio.us/~bcallah/mg/mg-20161016.tar.gz
+http://devio.us/~bcallah/mg/mg-20161104.tar.gz
--- version.c
+++ version.c
@@ -13,7 +13,7 @@
 
 #include "def.h"
 
-const char	version[] = "Mg 2a portable 20161016";
+const char	version[] = "Mg 2a portable 20161104";
 
 /*
  * Display the version. All this does
